e85b77cb04ac76df68d203a95b82dde2ee2a8e41
Tylar <murray.tylar@gmail.com>
2013-03-17 18:26:32 -0400

    almost working, but strange animation bug persists. about to do a rewrite of those files

--- JS/animateAvatar.js
+++ JS/animateAvatar.js
@@ -4,11 +4,11 @@ var maxAvatars = 1000;
 function oneFrameAll() {
 	for (var i=1 ; i < maxAvatars ; i++){
 		var currentAvatarID = "avatar" + i;
-		console.log(currentAvatarID);
+		//console.log(currentAvatarID);
 		currentAvatar = document.getElementById(currentAvatarID); 
-		console.log(currentAvatar);
+		//console.log(currentAvatar);
 		if( currentAvatar == null ) {
-			console.log("all avatars animated");
+			//console.log("all avatars animated");
 			return;
 		} else {
 			//currentAvatar.src = "images/animations/running/0.png";
@@ -18,18 +18,28 @@ function oneFrameAll() {
 }
 
 // move given image one frame
-function nextFrame(avatarElement){
+function nextFrame(avatarElementToChange){
 	//get old src (minus leading '?/images/animations/' (len=18) and file extension (len=4))
-	var fullOldSrc = avatarElement.src.split("/images/animations/")[1]; 
-	var oldSrc = fullOldSrc.substr(0,fullOldSrc.length-4);	//remove extension
-	var animDir   =          oldSrc.split("/")[0]  + '/';
-	var newFrameN = parseInt(oldSrc.split("/")[1]) + 1;
-	var newSrc = "images/animations/"+animDir+newFrameN+".png";
+	//var fullOldSrc = avatarElement.src.split("/images/animations/")[1]; 
+	splitSrc = avatarElementToChange.src.split("/");
+	//console.log(splitSrc);
+	//[0] = "http:",
+	//[1] = "",
+	//[2] = "localhost:8000",
+	//[3] = "images",
+	//[4] = "ani_vSmall",
+	//[5] = "running",
+	//[6] = "0.png"
+	//I don't really need 0-2, 3-5 need to stay the same, 6 needs to be changed
+	oldFrameN = splitSrc[6].substr(0,splitSrc[6].length-4);	//remove extension
+	var newFrameN = parseInt(oldFrameN) + 1;
+	var newSrc = splitSrc[3] +'/'+ splitSrc[4] +'/'+ splitSrc[5] +'/'+ newFrameN + ".png";
 
 	if ( ! UrlExists(newSrc) ){	//if animation not found
-		newSrc="images/animations/"+animDir+"0.png";//reset to 0 animation
+		 newSrc = splitSrc[3] +'/'+ splitSrc[4] +'/'+ splitSrc[5]+'/'+"0.png";//reset to 0 animation
 	}
-	avatarElement.src=newSrc;
+	avatarElementToChange.src=newSrc;
+	console.log(avatarElementToChange.id+" frame changed to " + avatarElementToChange.src);
 } 
 
 // check for file exists at url
--- JS/insertImage.js
+++ JS/insertImage.js
@@ -1,10 +1,24 @@
-function insertImage() {
-	for(var i = 1; i < 1000 ; i++){
+function insertImage(animationSize) {
+	var MAX_AVATARS = 1000;
+	for(var i = 1; i < MAX_AVATARS ; i++){
 		var newAvatarId = 'avatar'+i;
 		if(document.getElementById(newAvatarId) == null) {	//if avatar does not yet exist
 			//insert new avatar
-			document.getElementById('avatars').innerHTML+="<image id='"+newAvatarId+"' src='images/animations/running/0.png' />";
+			document.getElementById('avatars').innerHTML+="<image id='"+newAvatarId+"' src='images/"+animationSize+"/running/0.png' />";
+
+			//schedule first frame change
+			newAvatarElement = document.getElementById(newAvatarId);
+			//avatarNum = avatarElement.id.split('avatar')[1];	//remove 'avatar' at front of id
+			scheduleNextFrame(newAvatarElement);// call animation nextFrame scheduler
+			console.log(newAvatarId + " added");
 			break;	//exit the for loop
 		}
+	}
+}
+
+function insertAvatars(){
+	nAvatars = 1;
+	for(var i = 0; i < nAvatars ; i++){
+		insertImage(animationSize);
 	}
 }
