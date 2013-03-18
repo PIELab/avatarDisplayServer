3cd13d0adc508a9141ec1a4cad38a3c669f0d75d
Tylar <murray.tylar@gmail.com>
2013-03-17 23:44:08 -0400

    added multi-size images, animation FINALLY fixed.

--- JS/animateAvatar.js
+++ JS/animateAvatar.js
@@ -5,7 +5,7 @@ function oneFrameAll() {
 	for (var i=1 ; i < maxAvatars ; i++){
 		var currentAvatarID = "avatar" + i;
 		//console.log(currentAvatarID);
-		currentAvatar = document.getElementById(currentAvatarID); 
+		var currentAvatar = document.getElementById(currentAvatarID); 
 		//console.log(currentAvatar);
 		if( currentAvatar == null ) {
 			//console.log("all avatars animated");
@@ -18,10 +18,11 @@ function oneFrameAll() {
 }
 
 // move given image one frame
-function nextFrame(avatarElementToChange){
+function nextFrame(avatarIdToChange){
 	//get old src (minus leading '?/images/animations/' (len=18) and file extension (len=4))
 	//var fullOldSrc = avatarElement.src.split("/images/animations/")[1]; 
-	splitSrc = avatarElementToChange.src.split("/");
+	var avatarElementToChange = document.getElementById(avatarIdToChange);
+	var splitSrc = avatarElementToChange.src.split("/");
 	//console.log(splitSrc);
 	//[0] = "http:",
 	//[1] = "",
@@ -31,22 +32,32 @@ function nextFrame(avatarElementToChange){
 	//[5] = "running",
 	//[6] = "0.png"
 	//I don't really need 0-2, 3-5 need to stay the same, 6 needs to be changed
-	oldFrameN = splitSrc[6].substr(0,splitSrc[6].length-4);	//remove extension
+	var oldFrameN = splitSrc[6].substr(0,splitSrc[6].length-4);	//remove extension
 	var newFrameN = parseInt(oldFrameN) + 1;
 	var newSrc = splitSrc[3] +'/'+ splitSrc[4] +'/'+ splitSrc[5] +'/'+ newFrameN + ".png";
 
-	if ( ! UrlExists(newSrc) ){	//if animation not found
+	if ( ! UrlExists(newSrc,newFrameN) ){	//if animation not found
 		 newSrc = splitSrc[3] +'/'+ splitSrc[4] +'/'+ splitSrc[5]+'/'+"0.png";//reset to 0 animation
 	}
 	avatarElementToChange.src=newSrc;
-	console.log(avatarElementToChange.id+" frame changed to " + avatarElementToChange.src);
+	console.log(avatarElementToChange.id + " frame changed to " + avatarElementToChange.src);
 } 
 
 // check for file exists at url
-function UrlExists(url){
+function UrlExists(url, frameN){
+	//this is a quick hack which works much better:
+	var maxFrames = 11;
+	if (frameN > maxFrames){	//TODO: can get this from url (as well as animation if desired)
+		return false;
+	} else{
+		return true;
+	}
+	//this is the generalized way:
+/*
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
     return http.status!=404;
+*/
 }
 
